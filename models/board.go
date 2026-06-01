1eaab4cad419c1ca61f7eb33aaf8564bec871e2e
irpanzy <irfanoputra27@gmail.com>
2026-06-01 21:45:51 +0700

    feat: add board model with fields for internal ID, public ID, title, description, owner ID, owner public ID, timestamps, and due date

---
+++ models/board.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Board struct {
+	InternalID    int64          `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
+	PublicID      uuid.UUID      `json:"public_id" db:"public_id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	Title         string         `json:"title" db:"title" gorm:"not null"`
+	Description   string         `json:"description" db:"description"`
+	OwnerID       int64          `json:"owner_id" db:"owner_id" gorm:"not null;index"`
+	OwnerPublicID uuid.UUID      `json:"owner_public_id" db:"owner_public_id" gorm:"not null;index"`
+	CreatedAt     time.Time      `json:"created_at" db:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt     time.Time      `json:"updated_at" db:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `json:"-" gorm:"index"`
+	DueDate       *time.Time     `json:"due_date,omitempty" db:"due_date"`
+}
--- models/user.go
+++ models/user.go
@@ -8,13 +8,13 @@ import (
 )
 
 type User struct {
-	InternarlID int64          `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
-	PublicID    uuid.UUID      `json:"public_id" db:"public_id" gorm:"type:uuid;default:uuid_generate_v4()"`
-	Name        string         `json:"name" db:"name" gorm:"not null"`
-	Email       string         `json:"email" db:"email" gorm:"unique;not null"`
-	Password    string         `json:"password" db:"password" gorm:"not null"`
-	Role        string         `json:"role" db:"role" gorm:"not null"`
-	CreatedAt   time.Time      `json:"created_at" db:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time      `json:"updated_at" db:"updated_at" gorm:"autoUpdateTime"`
-	DeletedAt   gorm.DeletedAt `json:- gorm:"index"`
+	InternalID int64          `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
+	PublicID   uuid.UUID      `json:"public_id" db:"public_id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	Name       string         `json:"name" db:"name" gorm:"not null"`
+	Email      string         `json:"email" db:"email" gorm:"unique;not null"`
+	Password   string         `json:"-" db:"password" gorm:"not null"`
+	Role       string         `json:"role" db:"role" gorm:"not null"`
+	CreatedAt  time.Time      `json:"created_at" db:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt  time.Time      `json:"updated_at" db:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"index"`
 }

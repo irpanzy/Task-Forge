0b6d08fcbdf8cdd6df40f9fa30784789504b6d1f
irpanzy <irfanoputra27@gmail.com>
2026-06-02 21:17:39 +0700

    feat: add unique index to CardAssignee and CardLabel models

--- models/card_label.go
+++ models/card_label.go
@@ -1,6 +1,6 @@
 package models
 
 type CardLabel struct {
-	CardID  int64 `json:"card_id" db:"card_id" gorm:"not null;index"`
-	LabelID int64 `json:"label_id" db:"label_id" gorm:"not null;index"`
+	CardID  int64 `json:"card_id" db:"card_id" gorm:"not null;index;uniqueIndex:idx_card_label"`
+	LabelID int64 `json:"label_id" db:"label_id" gorm:"not null;index;uniqueIndex:idx_card_label"`
 }

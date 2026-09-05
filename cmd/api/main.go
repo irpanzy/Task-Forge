b950c3d190ad320c0cc02c0206ff721355e0d64f
irpanzy <irfanoputra27@gmail.com>
2026-09-05 22:28:47 +0700

    feat(api): integrate middleware, dependency injection, and route setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,27 +4,45 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/irpanzy/Task-Forge/internal/config"
+	"github.com/irpanzy/Task-Forge/internal/controller"
+	"github.com/irpanzy/Task-Forge/internal/middleware"
+	"github.com/irpanzy/Task-Forge/internal/repository"
+	"github.com/irpanzy/Task-Forge/internal/route"
+	"github.com/irpanzy/Task-Forge/internal/service"
+	"github.com/irpanzy/Task-Forge/pkg/response"
 )
 
 func main() {
-	// 1. Load environment variables & database connection
+	// 1. Load config & connect to Neon PostgreSQL
 	config.LoadEnv()
 	config.ConnectDB()
 
-	// 2. Initialize Fiber app
+	// 2. Inisialisasi Fiber app
 	app := fiber.New()
 
-	// Base health check route
+	// 3. Global Middlewares
+	app.Use(logger.New())
+	app.Use(middleware.NewCORS())
+	app.Use(middleware.NewCSRF())
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
+		return response.Success(c, fiber.StatusOK, "TaskForge API is running", fiber.Map{
 			"app":     "TaskForge API",
-			"status":  "running",
 			"version": "1.0.0",
 		})
 	})
 
-	// 3. Start server
+	// 4. Dependency Injection (Wiring Layer)
+	userRepo := repository.NewUserRepository(config.DB)
+	userService := service.NewUserService(userRepo)
+	authController := controller.NewAuthController(userService)
+
+	// 5. Setup Routes
+	route.SetupRoutes(app, authController)
+
+	// 6. Start Server
 	port := config.AppConfig.Port
 	if port == "" {
 		port = "3000"
